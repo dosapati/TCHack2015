136950a110270429c6e03767c46a80db2fb6403c
Hari Dosapati <dosapati@yahoo.com>
2015-05-02 22:59:10 -0400

    youtube api integration

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var express = require('express');
 var app = express();
 
 var google = require('google')
-
+var youtube = require ('youtube-feeds');
+youtube.httpProtocol = 'https';
+var async = require('async');
 
 
 app.get('/suggest/:searchText', function (req, res) {
@@ -19,8 +21,37 @@ app.get('/suggest/:searchText', function (req, res) {
   google.resultsPerPage = 25
   var nextCounter = 0;
   var finished = function () {
-    retObj.status="success";
-    res.send(retObj);
+
+    async.each(retObj.results, function(result, callback) {
+        var link = result.link;
+      if(link && link.indexOf('youtube') != -1){
+        console.log('video id -->',link.substring(link.indexOf('v=')+2,link.length));
+
+        youtube.video (link.substring(link.indexOf('v=')+2,link.length)).details(function(err,data){
+          //console.log('ytData -->',data);
+          result.ytData = data;
+          callback();
+        });
+      } else{
+        callback();
+      }
+    }, function(err){
+      retObj.status="success";
+      res.send(retObj);
+    });
+
+    /*for (var i = 0; i < retObj.results.length; ++i) {
+      if(retObj.results[i].link){
+        if(links[i].link.indexOf('youtube') != -1){
+          //get youtube data...
+          console.log('video id -->',links[i].link.substring(links[i].link.indexOf('v=')+2,links[i].link.length));
+          youtube.video (links[i].link.substring(links[i].link.indexOf('v=')+2,links[i].link.length)).details (console.log);
+        }
+      }
+      //console.log(links[i].title + ' - ' + links[i].link) // link.href is an alias for link.link
+      //console.log(links[i].description + "\n")
+    }*/
+
   }
   console.log('searchText :: --->',req.params.searchText);
   google(req.params.searchText, function (err, next, links){
@@ -29,6 +60,11 @@ app.get('/suggest/:searchText', function (req, res) {
     for (var i = 0; i < links.length; ++i) {
       if(links[i].link){
       retObj.results.push({title:links[i].title,link:links[i].link,description:links[i].description});
+        if(links[i].link.indexOf('youtube') != -1){
+          //get youtube data...
+          console.log('video id -->',links[i].link.substring(links[i].link.indexOf('v=')+2,links[i].link.length));
+          //youtube.video (links[i].link.substring(links[i].link.indexOf('v=')+2,links[i].link.length)).details (console.log);
+        }
       }
       //console.log(links[i].title + ' - ' + links[i].link) // link.href is an alias for link.link
       //console.log(links[i].description + "\n")
